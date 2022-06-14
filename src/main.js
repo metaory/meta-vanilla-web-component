cfa329cdb6ae71fae1fd9afa789f86d296367ee5
metaory <metaory@gmail.com>
2022-06-14 23:16:22 +0800

    cleanups

--- src/main.js
+++ src/main.js
@@ -5,14 +5,10 @@ import fetchRates from './fetchRates.js'
 let inputTimeout
 const updateRates = async (bounce = 1000) => {
   const { srcSymbol, dstSymbol, srcAmount } = currencyProxy
-
   clearTimeout(inputTimeout)
   inputTimeout = setTimeout(async () => {
-    document.querySelector('loader-modal').show(true)
-
     const { rates } = await fetchRates(srcSymbol, dstSymbol, srcAmount)
     currencyProxy.dstAmount = (rates[dstSymbol] * srcAmount).toFixed(2)
-
   }, bounce)
 }
 
