50e1cbc88b1e55e56ea0c363bb38cd5ea33bfb99
metaory <metaory@gmail.com>
2022-06-16 12:21:13 +0800

    added a simple cache layer

--- src/fetchRates.js
+++ src/fetchRates.js
@@ -1,17 +1,33 @@
+// TODO consider sessionStorage instead
+const setCache = (src, dst, value) =>
+  localStorage.setItem(`${src}-${dst}`, value)
+const loadCache = (src, dst) =>
+  localStorage.getItem(`${src}-${dst}`)
+
 export default function (src, dst, amount = 1) {
   console.log('FETCH RATES:', { src, dst, amount })
 
   const loader = document.querySelector('loader-modal')
   const alert = document.querySelector('alert-component')
 
+  // Attempting Cache
+  const cachedValue = loadCache(src, dst)
+  if (cachedValue) {
+    loader.show(false)
+    alert.show('Fetched from Cache!', 'var(--success)')
+    return { rates: { [dst]: cachedValue } }
+  }
+
   return new Promise((resolve) => {
     loader.show(true)
-    /* DEBUG MODE */
+
+    /* * DEBUG MODE * */
     // return setTimeout(() => {
     //   loader.show(false)
     //   resolve({ rates: { [dst]: (Math.random() * 1000).toFixed(2) } })
     //   alert.show('Fetched!', 'var(--success)')
     // }, 1000)
+    /* * ********** * */
 
     fetch(`https://api.apilayer.com/fixer/latest?base=${src}&symbols=${dst}&amount=${amount}`, {
       headers: {
@@ -24,10 +40,15 @@ export default function (src, dst, amount = 1) {
       .then(response => {
         if (response.ok) {
           alert.show('Fetched!', 'var(--success)')
-          return resolve(response.json())
+          const promise = response.json()
+          promise.then((result) => {
+            setCache(src, dst, result.rates[dst])
+            return resolve(result)
+          })
+        } else {
+          const errors = { 429: '429 Too Many Requests, falling back to dummy response.' }
+          throw new Error(errors[response.status] || response.status)
         }
-        const errors = { 429: '429 Too Many Requests, falling back to dummy response.' }
-        throw new Error(errors[response.status] || response.status)
       })
       .catch(err => {
         console.error('E!:', err)
