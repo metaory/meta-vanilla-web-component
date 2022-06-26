d3362e597ca4fe9400d5e86cdf278e3a4adb7c36
metaory <metaory@gmail.com>
2022-06-26 13:35:03 +0800

    loader fetchAndParse turned async

--- src/loader.js
+++ src/loader.js
@@ -1,18 +1,16 @@
 
 window.loadComponent = (() => {
-  const fetchAndParse = (URL) => {
-    return fetch(URL).then((response) => {
-      return response.text()
-    }).then((html) => {
-      const parser = new DOMParser()
-      const document = parser.parseFromString(html, 'text/html')
-      const head = document.head
-      const template = head.querySelector('template')
-      const style = head.querySelector('style')
-      const script = head.querySelector('script')
+  const fetchAndParse = async (URL) => {
+    const html = await fetch(URL).then(response => response.text())
 
-      return { template, style, script }
-    })
+    const parser = new DOMParser()
+    const document = parser.parseFromString(html, 'text/html')
+    const head = document.head
+    const template = head.querySelector('template')
+    const style = head.querySelector('style')
+    const script = head.querySelector('script')
+
+    return { template, style, script }
   }
 
   const getSettings = async ({ template, style, script }) => {
